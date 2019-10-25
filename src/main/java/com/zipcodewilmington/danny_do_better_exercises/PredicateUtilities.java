3fb62482bbb3bedd6051c846a16687d08028654e
Brandon Chambers <brandonmchambers36@gmail.com>
2019-10-25 09:21:54 -0400

    Math complete

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -10,8 +10,11 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+        if (x > y ) {
+        }
+        return true;
     }
+
 
     /**
      * @param x
@@ -19,7 +22,9 @@ public class PredicateUtilities {
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
-        return null;
+        if (x < y ) {
+        }
+        return true;
     }
 
     /**
@@ -28,31 +33,37 @@ public class PredicateUtilities {
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
-    }
+        if (x >= y) {
 
+        }
+        return true;
+    }
     /**
      * @param x
      * @param y
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
-    }
+        if (x <= y) {
+
+        }
+            return true;
+        }
+
 
 
     /**
      * @return true
      */
     public Boolean returnTrue() {
-        return null;
+        return true;
     }
 
     /**
      * @return false
      */
     public Boolean returnFalse() {
-        return null;
+        return false;
     }
 
 }
